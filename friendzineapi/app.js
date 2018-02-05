5361db4973064a064b22c724272eb9d5e6ff35af
Your Name <controlsltrapidfire@gmail.comm>
2018-02-05 15:48:39 -0600

    adding code from tutorial. I hope this all works ok.

--- friendzineapi/app.js
+++ friendzineapi/app.js
@@ -11,6 +11,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+var dbConfig = require('./config/database.config.js');
+var mongoose = require('mongoose');
+
+mongoose.connect(dbConfig.url, {
+    useMongoClient: true
+});
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// // define a simple route
+// app.get('/', function(req, res){
+//     res.json({"message": "friendZine. Taking out network back!"});
+// });
+//
+// // ........
+//
+// // Require Notes routes
+// require('./app/routes/note.routes.js')(app);
+//
+// // ........
+
+// listen for requests
+app.listen(3000, function(){
+    console.log("Server is listening on port 3000");
+});
+
+app.use('/api/v1/comment', users);
 
 module.exports = app;
---
+++ friendzineapi/controllers/friendZone.controller.js
@@ -0,0 +1,69 @@
+var Note = require('../models/comment.model.js');
+
+exports.create = function(req, res) {
+    // Create and Save a new Note
+    if(!req.body.content) {
+        res.status(400).send({message: "Comment can not be empty"});
+    }
+     var comment = new Comment({content: req.body.content});
+
+     note.comment(function(err, data) {
+        console.log(data);
+        if(err) {
+            console.log(err);
+            res.status(500).send({message: "Some error occurred while creating the Comment."});
+        } else {
+            res.send(data);
+        }
+    });
+
+exports.findAll = function(req, res) {
+    // Retrieve and return all notes from the database.
+    Comment.find(function(err, notes){
+        if(err) {
+            res.status(500).send({message: "Some error occurred while retrieving comments."});
+        } else {
+            res.send(comments);
+        }
+    });
+};
+exports.findOne = function(req, res) {
+    // Find a single note with a noteId
+    Comment.findById(req.params.commentId, function(err, data) {
+       if(err) {
+           res.status(500).send({message: "Could not retrieve comment with id " + req.params.commentId});
+       } else {
+           res.send(data);
+       }
+    });
+};
+
+exports.update = function(req, res) {
+    // Update a comment identified by the commentId in the request
+    Comment.findById(req.params.commentId, function(err, comment) {
+        if(err) {
+            res.status(500).send({message: "Could not find a comment with id " + req.params.commentId});
+        }
+
+        comment.content = req.body.content;
+
+        comment.save(function(err, data){
+            if(err) {
+                res.status(500).send({message: "Could not update comment with id " + req.params.commentId});
+            } else {
+                res.send(data);
+            }
+        });
+    });
+};
+
+exports.delete = function(req, res) {
+    // Delete a note with the specified noteId in the request
+    Comment.remove({_id: req.params.commentId}, function(err, data) {
+        if(err) {
+            res.status(500).send({message: "Could not delete comment with id " + req.params.id});
+        } else {
+            res.send({message: "Comment deleted successfully!"})
+        }
+    });
+};
