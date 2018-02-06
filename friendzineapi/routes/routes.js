e03232e4860a3e865e465bcf600ed66eb2e48083
Your Name <controlsltrapidfire@gmail.comm>
2018-02-06 10:10:29 -0600

    added to routes

--- friendzineapi/routes/routes.js
+++ friendzineapi/routes/routes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
 module.exports = function(app) {
 
     var Comment = require('../controllers/comment.controller.js');
