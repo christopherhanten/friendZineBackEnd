a94ef81532e92d8480089688f99602754c8b3c59
Your Name <controlsltrapidfire@gmail.comm>
2018-02-07 12:03:22 -0600

    added routes for addMusic and addLink

---
+++ friendzineapi/routes/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
+module.exports = function(app) {
+
+    var comment = require('../controllers/comment.controller.js');
+
+    // Create a new Comment
+    app.post('/comments', comment.create);
+
+    // Retrieve all Comments
+    app.get('/comments', comment.findAll);
+
+    // Retrieve a single Note with CommentId
+    app.get('/comments/:commentId', comment.findOne);
+
+    // Update a Note with CommentId
+    app.put('/comments/:commentId', comment.update);
+
+    // Delete a Note with CommentId
+    app.delete('/comment/:commentId', comment.delete);
+
+    // Add a comment from addMusic
+    app.get('/addMusic' (req,res) {
+      res.render('/addMusic')
+    });
+    app.post('/addMusic', comment.create);
+
+    // Add a comment from addLink
+    app.get('/addLink' (req,res) {
+      res.render('/addLink')
+    });
+    app.post('/addLink', comment.create);
+}
